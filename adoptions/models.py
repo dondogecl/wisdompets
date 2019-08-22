14453faa9b711482f8ecd7283dac073f1dceae71
alejandro.acevedo <alejandro.acevedo@zentagroup.com>
2019-08-22 12:45:35 -0400

    migracion DB: OK. Creado superusuario.

--- adoptions/models.py
+++ adoptions/models.py
@@ -3,16 +3,16 @@ from django.db import models
 # Create your models here.
 class Pet(models.Model):
     SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
-    
-    name = models.CharField(max_lenght=100)
-    submitter = models.CharField(max_lenght=100)
-    species = models.CharField(max_lenght=50)
-    breed = models.CharField(max_lenght=50, blank=True)
+
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=50)
+    breed = models.CharField(max_length=50, blank=True)
     description = models.TextField()
     sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
-    submisssion_date = models.DateTimeField()
+    submission_date = models.DateTimeField()
     age = models.IntegerField(null=True)
     vaccinations = models.ManyToManyField('Vaccine', blank=True)
-    
+
 class Vaccine(models.Model):
     name = models.CharField(max_length=50)
