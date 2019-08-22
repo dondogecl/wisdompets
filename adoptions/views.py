60e58930c712e382dd67b4bd2d94fa5afc3f3357
alejandro.acevedo <alejandro.acevedo@zentagroup.com>
2019-08-22 13:28:20 -0400

    creadas rutas URL y vistas basicas

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<p>Home view</p>')
+
+def pet_detail(request, id):
+    return HttpResponse('<p>Pet Detail view with the ID {}</p>'.format(id))
