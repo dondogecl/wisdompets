1f6128743b18f186bef9f312905d1b2f2cdf80b8
alejandro.acevedo <alejandro.acevedo@zentagroup.com>
2019-08-22 15:14:48 -0400

    views: implementado render basico

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Pet
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<p>Home view</p>')
+    #return HttpResponse('<p>Home view</p>')
+    pets = Pet.objects.all()
+    # render
+    return render(request, 'home.html', {'pets' : pets}) #obj request, template a usar, diccionario con la data que usaremos
+    
 
 def pet_detail(request, id):
-    return HttpResponse('<p>Pet Detail view with the ID {}</p>'.format(id))
+    #return HttpResponse('<p>Pet Detail view with the ID {}</p>'.format(id))
+    try:
+        pet = Pet.objects.get(id=id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet not found')
+    return render(request, 'pet_detail.html', {'pet': pet})
